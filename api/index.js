3db84330a37b8b6d23c618915b1e54aa19f72bb7
Jean Rondon <jeanrdev@gmail.com>
2023-02-05 16:21:21 -0400

    creating models of my api

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
-console.log('Hola');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('DB is working'))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => console.log('backend server is running...'));
