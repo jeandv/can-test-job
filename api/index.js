60e2240fa21871d50f151f1a1fc9051912387c4f
Jean Rondon <jeanrdev@gmail.com>
2023-02-05 18:29:22 -0400

    creating routes and completing the login/register config with JWT

--- api/index.js
+++ api/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+
+const port = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', false);
+
 dotenv.config();
 
 const app = express();
@@ -10,4 +17,8 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('DB is working'))
   .catch((err) => console.log(err));
 
-app.listen(process.env.PORT || 5000, () => console.log('backend server is running...'));
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+
+app.listen(port, () => console.log('backend server is running...', port));
