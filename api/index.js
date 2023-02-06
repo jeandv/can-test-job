cddd7e56f35b11af4fd16aecd4d1af6ec1e91bcf
Jean Rondon <jeanrdev@gmail.com>
2023-02-05 23:16:02 -0400

    creating and adding stripe configuration

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const eventsRoute = require('./routes/event');
 const cartRoute = require('./routes/cart');
-
+const orderRoute = require('./routes/order');
 
 const port = process.env.PORT || 5000;
 
@@ -29,5 +29,7 @@ app.use('/api/users', userRoute);
 app.use('/api/events', eventsRoute);
 
 app.use('/api/carts', cartRoute);
+
+app.use('/api/orders', orderRoute);
 
 app.listen(port, () => console.log('backend server is running...', port));
